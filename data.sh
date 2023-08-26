74cb4314642777018c325065012cfd254d9c2694
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-08-26 20:32:34 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt-get full-upgrade -y
+sudo apt-get install -y python3-pip
+sudo git clone https://github.com/ajay77777777/flask-library-app.git
+cd flask-library-app/
+pip3 install -r requirements.txt
+python3 app.py
